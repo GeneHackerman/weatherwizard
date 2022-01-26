78ee4a692ea63971c2c41dc8685569e8f7a658ee
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-01-26 16:12:23 -0500

    adding search functionality and local storage

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -23,6 +23,48 @@ var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
 
 // check if search history exists when page loads
-// initializeHistory();
-// showClear();
+initializeHistory();
+showClear();
+
+// will search for city and display the current weather conditions 
+$(document).on("submit", function() {
+    event.preventDefault();
+
+    // searches for city 
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);
+    searchCityInput.val("");
+});
+
+// upon submit, city will be added to history
+searchCityButton.on("click", function(event) {
+    event.preventDefault();
+
+    // grab value entered in search bar
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);
+    searchCityInput.val("");
+}); 
+
+// clear sidebar of past searche queries
+clearHistoryButton.on("click", function() {
+    // empty out city list array
+    cityList = [];
+    // update city list history in local storage
+    listArray();
+
+    $(this).addClass("hide");
+});
+
+// clicking on search history sidebar list will display the info of that city clicked
+searchHistoryList.on("click", "li.city-btn", function(event) {
+    //console.log($(this).data("value"));
+    var value = $(this).data("value");
+    currentConditionsRequest(value);
+    searchHistory(value);
+});
 
